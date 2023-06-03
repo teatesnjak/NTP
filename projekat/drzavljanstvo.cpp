2896c211aec06e31a4fb281b057e68c775c01be4
teatesnjak <126908624+teatesnjak@users.noreply.github.com>
2023-06-03 09:35:59 +0200

    Update drzavljanstvo.cpp

--- projekat/drzavljanstvo.cpp
+++ projekat/drzavljanstvo.cpp
@@ -1,10 +1,11 @@
 
 void drzavljanstvo(gradjanin g[], int brojacGradjana){
-string broj;
+string broj; //ovom varijablom povezacemo jmbg iz struct gradjanin sa odgovarajucim jmbg gradjanina
 cout << "\tUnesite Jedinstveni maticni broj: ";
 cin >> broj;
 for (int i=0; i<brojacGradjana; i++){
-if(g[i].jmbg==broj){
+if(g[i].jmbg==broj) //string broj postaje jmbg iz struct gradjanin
+{
 	cout << "---------------------------------------------------\n";
 	cout << "\tPrezime: "<< g[i].prezime << endl;
 	cout << "\tIme: " << g[i].ime << endl;
@@ -14,5 +15,5 @@ if(g[i].jmbg==broj){
 	cout << "---------------------------------------------------\n";
 }
 else{cout << "\tNemamo zabiljezen taj Jedinstveni maticni broj. \n ";}
-}
+} // ako ima jmbg, ispisuje sav info, ako nema, vrati na meni uz odgovarajucu poruku
 }
