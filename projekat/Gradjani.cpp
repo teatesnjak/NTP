01b2cf427030a8b99ec570bae6657a9ac1ae3c6c
teatesnjak <126908624+teatesnjak@users.noreply.github.com>
2023-06-03 09:16:54 +0200

    Update Gradjani.cpp

--- projekat/Gradjani.cpp
+++ projekat/Gradjani.cpp
@@ -2,18 +2,19 @@
 #include <cstdlib>
 #include <string>
 #include <fstream>
-#include "unos.cpp"
-#include "ispis.cpp"
-#include "rodniList.cpp"
-#include "drzavljanstvo.cpp"
+#include "unos.cpp" //pozivamo odvojeni unos.cpp fajl
+#include "ispis.cpp" //pozivamo odvojeni ispis.cpp fajl
+#include "rodniList.cpp" //pozivamo odvojeni rodniList.cpp fajl
+#include "drzavljanstvo.cpp" //pozivamo odvojeni drzavljanstvo.cpp fajl
 using namespace std;
 
 int main(){
-int odabir=0;
-gradjanin g[100];
-brojacGradjana=0;
+int odabir=0; //koristimo u do while petlji za odabir 1 od 4 opciju ponudjenih
+gradjanin g[100]; //od struct gradnjanin
+brojacGradjana=0; //pohranjuje broj gradjana iz unosa
 
 do{
+//nas meni, osnovni izgled stranice
 cout << "---------------------------------------------------\n";
 cout << "\t1. Unos gradjana \n";
 cout << "\t2. Informacija o gradjanima \n";
@@ -24,23 +25,23 @@ cout << "\tOdaberite sta zelite: ";
 cin >> odabir;
 
 if (odabir==1){
-unos(g, brojacGradjana);
-system("pause");}
+    unos(g, brojacGradjana); //uzima podatke iz struct gradjanin i funkcije unos i omogucava unos podataka naseg gradjanina
+    system("pause");}
 
 if (odabir==2){
-ispis(g, brojacGradjana);
-system("pause");}
+    ispis(g, brojacGradjana); //uzima podatke iz struct gradjanin i funkcije ispis te ispisuje sve o gradjaninu nakon unosa JMBG-a
+    system("pause");}
 
 if (odabir==3){
-    rodniList (g, brojacGradjana);
+    rodniList (g, brojacGradjana); //uzima podatke iz struct gradjanin i funkcije rodniList te ispisuje trazene podatke o gradjaninu nakon unosa JMBG-a
     system("pause");}
     
 if (odabir==4){
-    drzavljanstvo (g, brojacGradjana);
+    drzavljanstvo (g, brojacGradjana); //uzima podatke iz struct gradjanin i funkcije drzavljanstvo te ispisuje trazene podatke o gradjaninu nakon unosa JMBG-a
     system("pause");}
 
-system("CLS");
-} while(odabir!=0);
+system("CLS"); //obrise ekran od prethodnog odabira
+} while(odabir!=0); 
 
 return 0;
 }
